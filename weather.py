60abf79b94cfac37e27e614ff56b60d24230a6f0
yoav weber <yoav.weber@code.berlin>
2019-05-14 20:21:53 +0200

    changes

--- weather.py
+++ weather.py
@@ -11,14 +11,14 @@ app.secret_key = 'test_key'
 
 
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/', methods=('GET'))
 def hello_world():
-    if request.method == 'POST':
-        city = request.form['city1']
+    if request.method == 'GET':
+        city = request.args.get('city')
         dic_items = {str(get_city_temp(city)):city} # I had to convert to string since the cookie dosn't accept float/int
         session.update(dic_items)
-        if len(dict(session)) == 2:
-            highest_temp_dic = convert_float(dict(session))
+        if len(dict(session)) == 5:
+            highest_temp_dic = convert_float(dict(session)) # here i convert it back to float
             hottest_temp = max(highest_temp_dic)
             hottest_city = highest_temp_dic[hottest_temp]
             return '<b>{}</b> is the hottest city with <b>{}<b> degrees'.format(hottest_city,hottest_temp)
